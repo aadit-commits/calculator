ccaa275543de799d71021fb7eec6432e4dc00164
Aadit <mail2aaditsarhadi@gmail.com>
2024-09-05 21:20:11 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout << "Enter a number : " << endl;
+    cin >> a;
+    cout << "Enter other number : " << endl;
+    cin >> b;
+    cout << "Sum of the numbers is : " << a+b << endl;
+    cout << "Subtraction of the numbers is : " << a-b << endl;
+    cout << "Product of the numbers is : " << a*b << endl;
+    cout << "Division of the numbers is : " << a/b << endl;
+    return 0;
+}
